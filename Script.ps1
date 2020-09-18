e33ce967f0f7253c477a3fde97eb1b4151c6a826
ATASH-BIZ-YEGANEH Anahita <anahita.atash-biz-yeganeh@sword-group.com>
2020-09-18 14:55:07 -0400

    migrate a OneDrive to new Tenant(from swordcanada to migrationLearning

--- Script.ps1
+++ Script.ps1
@@ -1,16 +1,13 @@
 <#
 $tenant = Connect-Site -Url "https://swordcanada-admin.sharepoint.com" -Browser
-$csvFile = "C:\Users\aatash-biz-yeganeh\oneDriveTest-ShareGate\url.csv"
-$table = Import-Csv $csvFile -Delimiter ","
+$File = "C:\Users\aatash-biz-yeganeh\oneDriveTest-ShareGate\url.csv"
+$table = Import-Csv $File -Delimiter ","
 foreach ($row in $table) {
     Get-OneDriveUrl -Tenant $tenant -Email $row.Email -ProvisionIfRequired -DoNotWaitForProvisioning
 }
 
 
-Write-Host "step 1"
-$mypassword = ConvertTo-SecureString "Annakjkj@75" -AsPlainText -Force
-$tenant = Connect-Site -Url "https://swordcanada-admin.sharepoint.com" -Username "anahita.atash-biz-yeganeh@swordcanada.onmicrosoft.com" -Password $mypassword
-Get-OneDriveUrl -Tenant $tenant -Email anahita.atash-biz-yeganeh@swordcanada.onmicrosoft.com -ProvisionIfRequired -DoNotWaitForProvisioning
+
 #>
 
 param (
@@ -24,11 +21,9 @@ param (
 )
  
 Import-Module Sharegate
-
+$table = Import-Csv $csvFile -Delimiter ","
 if($type.ToUpper() -eq "ONEDRIVE"){
     Write-Host "OneDrive - OneDrive migration"
-
-$table = Import-Csv $csvFile -Delimiter ","
 
 Set-Variable srcSite, dstSite, srcList, dstList
 foreach ($row in $table) {
@@ -45,13 +40,10 @@ foreach ($row in $table) {
  Add-SiteCollectionAdministrator -Site $dstSite
  $srcList = Get-List -Site $srcSite -Name "Documents"
  $dstList = Get-List -Site $dstSite -Name "Documents"
-
  if(!$srcList){
      Write-Host ("The source list " +$srcList+ " does not exist") 
-
  }
  else{
-
  Copy-Content -SourceList $srcList -DestinationList $dstList
 }
  Remove-SiteCollectionAdministrator -Site $srcSite
@@ -59,11 +51,8 @@ foreach ($row in $table) {
 }
 }
 
-
 if($type -eq "Server"){
     Write-Host "Server - OneDrive migration"
-#$csvFile = "C:\Users\aatash-biz-yeganeh\oneDriveTest-ShareGate\url.csv" 
-$table = Import-Csv -Path $csvFile -Delimiter ","
 Set-Variable dstSite, dstList
 foreach ($row in $table) {
     Clear-Variable dstSite
