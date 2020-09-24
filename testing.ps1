53845c7fd716ca2f30c40921e23aa5a5dba2c424
ATASH-BIZ-YEGANEH Anahita <anahita.atash-biz-yeganeh@sword-group.com>
2020-09-24 16:59:54 -0400

    clean up + Working_script.ps1 is final version that works for (server-OneDrive migration)

--- testing.ps1
+++
@@ -1,7 +0,0 @@
-$dstUsername = "anahita.atash-biz-yeganeh@swordcanada.onmicrosoft.com"
-$dstPassword = ConvertTo-SecureString "Annakjkj@75" -AsPlainText -Force
-[System.Management.Automation.PSCredential]$destinationMigrationCredentials = New-Object System.Management.Automation.PSCredential($dstUsername, $dstPassword)
-
-Connect-PnPOnline -url "https://swordcanada-admin.sharepoint.com" -Credentials $destinationMigrationCredentials
-
-Get-PnPUser  |Select-Object Title,Email | Export-Csv C:\Users\aatash-biz-yeganeh\oneDriveTest-ShareGate\swordcanada.csv 
